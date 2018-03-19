a482e3d27f0cd16210828757665571b6b1a3dd18
IndirasM <I.Maziukas@gmail.com>
2018-03-19 17:21:13 +0200

    Initial commit

---
+++ src/main/java/lt/swedbank/itacademy/carleasing/CarleasingApplication.java
@@ -0,0 +1,12 @@
+package lt.swedbank.itacademy.carleasing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarleasingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarleasingApplication.class, args);
+	}
+}
---
+++ src/test/java/lt/swedbank/itacademy/carleasing/CarleasingApplicationTests.java
@@ -0,0 +1,16 @@
+package lt.swedbank.itacademy.carleasing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarleasingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
