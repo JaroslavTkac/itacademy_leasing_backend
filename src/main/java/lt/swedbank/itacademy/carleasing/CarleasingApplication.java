caaad2c1303ec511ac7feff51ee016c149975f37
JaroslavTkac <jarektkaciuk@gmail.com>
2018-04-04 15:46:08 +0300

    Revert "heroku"
    This reverts commit c6fe048787f725664011fc57963904c369030d10.

--- src/main/java/lt/swedbank/itacademy/carleasing/CarleasingApplication.java
+++ src/main/java/lt/swedbank/itacademy/carleasing/CarleasingApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarLeasingApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(CarLeasingApplication.class, args);
 	}
