dd88a23df69eae35bbd03252a5168eb47ff3d4c1
JaroslavTkac <jarektkaciuk@gmail.com>
2018-04-04 15:43:36 +0300

    Revert "t"
    This reverts commit 9d060ef1ef88ce6bee934fb7bc4faa24bae2f0e6.

--- src/test/java/lt/swedbank/itacademy/carleasing/CarLeasingApplicationTests.java
+++ src/test/java/lt/swedbank/itacademy/carleasing/CarLeasingApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CarleasingApplicationTests {
+public class CarLeasingApplicationTests {
 
     @Test
     public void contextLoads() {
