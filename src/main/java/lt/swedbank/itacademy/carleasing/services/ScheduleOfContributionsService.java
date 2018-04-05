820e6d96c43232dc12d082433d2d0ff1895b5e42
JaroslavTkac <jarektkaciuk@gmail.com>
2018-04-05 15:16:53 +0300

    Schedule of Contribution code cleaning

--- src/main/java/lt/swedbank/itacademy/carleasing/services/ScheduleOfContributionsService.java
+++ src/main/java/lt/swedbank/itacademy/carleasing/services/ScheduleOfContributionsService.java
@@ -20,17 +20,10 @@ public class ScheduleOfContributionsService {
     public ScheduleOfContributionsResponse calculateScheduleOfContributions(@Valid ScheduleOfContributions schedule) {
         List<ScheduleOfContributionsPaymentData> paymentData = new ArrayList<>();
 
-        System.out.println("IMOKA 1");
-        System.out.println("----");
-
         BigDecimal notRedeemedAssetValue = schedule.getAssetPrice().subtract(schedule.getAdvancePaymentAmount());
-
-        System.out.println("Not Redeemed Asset Value = " + notRedeemedAssetValue);
 
         BigDecimal r = new BigDecimal((schedule.getMargin() / 100) / 12)
                 .round(new MathContext(16, RoundingMode.HALF_UP));
-
-        //System.out.println("r: " + r);
 
                                  // 1 - (1 / (1 + r)^m)
         BigDecimal denominator = (BigDecimal.ONE.subtract
@@ -52,11 +45,8 @@ public class ScheduleOfContributionsService {
                 // ( 1 - (1 / (1 + r)^m) ) / r
                 .divide(r, 16, RoundingMode.HALF_UP);
 
-        //System.out.println("denominator = " + denominator);
 
         BigDecimal totalMonthlyPaymentValue = notRedeemedAssetValue.divide(denominator, 16, RoundingMode.HALF_UP);
-
-        System.out.println("menesine Imoka = " + totalMonthlyPaymentValue);
 
         BigDecimal interest = BigDecimal.ONE.add
                 (
@@ -64,22 +54,16 @@ public class ScheduleOfContributionsService {
                 )
                 .round(new MathContext(16, RoundingMode.HALF_UP));
 
-        //System.out.println("Interest = " + interest);
 
         BigDecimal paymentWithInterest = notRedeemedAssetValue.multiply(interest)
                 .round(new MathContext(16, RoundingMode.HALF_UP));
-        System.out.println("Su palukanomis = " + paymentWithInterest);
-
 
 
         BigDecimal interestPayments = paymentWithInterest.subtract(notRedeemedAssetValue);
-        System.out.println("Menesine palukanu imoka = " + interestPayments);
 
         BigDecimal assetRedemptionFees = totalMonthlyPaymentValue.subtract(interestPayments);
-        System.out.println("Turto ispirkimo imoka = " + assetRedemptionFees);
 
         BigDecimal newNotRedeemedAssetValue = notRedeemedAssetValue.subtract(assetRedemptionFees);
-        System.out.println("Neispirkta turto verte = " + newNotRedeemedAssetValue);
 
         paymentData.add(new ScheduleOfContributionsPaymentData(
                 notRedeemedAssetValue,
@@ -87,17 +71,9 @@ public class ScheduleOfContributionsService {
                 interestPayments,
                 totalMonthlyPaymentValue
                 ));
-        System.out.println("----");
+
         for (int i = 0; i < schedule.getLeasePeriod() - 1; i++){
-            System.out.println("IMOKA " + (i + 2));
-            System.out.println("----");
-
             notRedeemedAssetValue = newNotRedeemedAssetValue;
-            System.out.println("Not Redeemed Asset Value = " + notRedeemedAssetValue);
-
-            //totalMonthlyPaymentValue = notRedeemedAssetValue.divide(denominator, 8, RoundingMode.HALF_UP);
-
-            System.out.println("menesine Imoka = " + totalMonthlyPaymentValue);
 
             interest = BigDecimal.ONE.add
                     (
@@ -105,26 +81,17 @@ public class ScheduleOfContributionsService {
                     )
                     .round(new MathContext(16, RoundingMode.HALF_UP));
 
-            //System.out.println("Interest = " + interest);
-
             paymentWithInterest = notRedeemedAssetValue.multiply(interest)
                     .round(new MathContext(16, RoundingMode.HALF_UP));
-            System.out.println("Su palukanomis = " + paymentWithInterest);
-
 
             interestPayments = paymentWithInterest.subtract(notRedeemedAssetValue)
                     .round(new MathContext(16, RoundingMode.HALF_UP));
-            System.out.println("Menesine palukanu imoka = " + interestPayments);
 
             assetRedemptionFees = totalMonthlyPaymentValue.subtract(interestPayments)
                     .round(new MathContext(16, RoundingMode.HALF_UP));
-            System.out.println("Turto ispirkimo imoka = " + assetRedemptionFees);
 
             newNotRedeemedAssetValue = notRedeemedAssetValue.subtract(assetRedemptionFees)
                     .round(new MathContext(16, RoundingMode.HALF_UP));
-            System.out.println("Neispirkta turto verte = " + newNotRedeemedAssetValue);
-
-            System.out.println("----");
 
             paymentData.add(new ScheduleOfContributionsPaymentData(
                     notRedeemedAssetValue,
@@ -133,8 +100,6 @@ public class ScheduleOfContributionsService {
                     totalMonthlyPaymentValue
             ));
         }
-
-        System.out.println("Payment data size: " + paymentData.size());
 
         return new ScheduleOfContributionsResponse(paymentData);
     }
