a3eeaa1680174649b14f86f9aaf2adb596679cb3
JaroslavTkac <jarektkaciuk@gmail.com>
2018-04-09 17:47:47 +0300

    Email sender

--- src/main/java/lt/swedbank/itacademy/carleasing/CarleasingApplication.java
+++ src/main/java/lt/swedbank/itacademy/carleasing/CarleasingApplication.java
@@ -4,9 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarleasingApplication {
+public class CarleasingApplication{
+
+//	@Autowired
+//	private EmailService emailService;
+//
+//	@Override
+//	public void run(ApplicationArguments applicationArguments) throws Exception {
+//		Mail mail = new Mail();
+//		mail.setFrom("majestic.car.leasing@gmail.com");
+//		mail.setTo("jarektkaciuk@gmail.com");
+//		mail.setSubject("Sending Simple Email with JavaMailSender Example");
+//		mail.setContent("This tutorial demonstrates how to send a simple email using Spring Framework.");
+//
+//		emailService.sendSimpleMessage(mail);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarleasingApplication.class, args);
+
 	}
+
+
 }
--- src/main/java/lt/swedbank/itacademy/carleasing/services/CompleteLeaseService.java
+++ src/main/java/lt/swedbank/itacademy/carleasing/services/CompleteLeaseService.java
@@ -2,12 +2,16 @@ package lt.swedbank.itacademy.carleasing.services;
 
 import lt.swedbank.itacademy.carleasing.beans.documents.CompleteLease;
 import lt.swedbank.itacademy.carleasing.beans.responses.CompleteLeaseResponse;
+import lt.swedbank.itacademy.carleasing.mail.EmailService;
+import lt.swedbank.itacademy.carleasing.mail.Mail;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 @Service
 public class CompleteLeaseService {
+
+    @Autowired
+    private EmailService emailService;
 
     @Autowired
     private LeaseService leaseService;
@@ -28,13 +32,28 @@ public class CompleteLeaseService {
             lease.getPrivateCustomer().setLeaseId(leaseId);
             lease.getPrivateCustomer().setId(new ObjectId());
             privateCustomerService.addNewPrivateCustomer(lease.getPrivateCustomer());
+            sendEmail(lease.getPrivateCustomer().getEmail(), String.valueOf(leaseId));
         }
         if(lease.getLease().getLeaseType().equals("Corporate")){
             lease.getCorporateCustomer().setLeaseId(leaseId);
             lease.getCorporateCustomer().setId(new ObjectId());
             corporateCustomerService.addNewCorporateCustomer(lease.getCorporateCustomer());
+            sendEmail(lease.getCorporateCustomer().getEmail(), String.valueOf(leaseId));
         }
 
         return new CompleteLeaseResponse(lease);
     }
+
+    private void sendEmail(String userEmail, String leaseId){
+        Mail mail = new Mail();
+        mail.setFrom("majestic.car.leasing@gmail.com");
+        mail.setTo(userEmail);
+        mail.setSubject("Majestic car lease ID");
+        mail.setContent("Your lease ID: " + leaseId + "\nWith this ID you can information and status of your lease.\n\nSincerely, Majestic Leasing");
+
+        emailService.sendSimpleMessage(mail);
+    }
+
+
+
 }
